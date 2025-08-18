81d72c75c6cd064c072c461ed150034153c0e17b
ShivuAH <shivanandhanchinal5@gmail.com>
2025-08-18 22:53:28 +0530

    Python minimal code

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -6,6 +6,7 @@ def main(args=None):
     rclpy.init(args=args)
     node = Node("py_test")
     node.get_logger().info("Hello world")
+    rclpy.spin(node)
     rclpy.shutdown()
 
 if __name__ == "__main__":
