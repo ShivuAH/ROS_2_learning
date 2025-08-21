3f329da1198743297bcb0b681d39dea2b03561e2
ShivuAH <shivanandhanchinal5@gmail.com>
2025-08-21 07:11:57 +0530

    Cpp node- minimal code

--- my_cpp_pkg/src/my_first_node.cpp
+++ my_cpp_pkg/src/my_first_node.cpp
@@ -1,1 +1,9 @@
 #include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char **argv){
+    rclcpp::init(argc,argv);
+    auto node =std::make_shared<rclcpp::Node>("cpp_test");
+    RCLCPP_INFO(node->get_logger(),"Heloo world");
+    rclcpp::shutdown();
+    return 0;
+}
